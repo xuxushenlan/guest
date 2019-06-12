bc2cd482badc83389145a11f7821600a0d4055df
xuxushenlan <515945334@qq.com>
2019-06-12 17:18:28 +0800

    add guest paginator

--- sign/views.py
+++ sign/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from sign.models import Event,Guest
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 
 # Create your views here.
@@ -26,8 +27,8 @@ def login_action(request):
 # 发布会管理
 @login_required
 def event_manage(request):
+    username = request.session.get('user', '')  # 读取浏览器session
     event_list = Event.objects.all()
-    username = request.session.get('user', '')      # 读取浏览器session
     return render(request, "event_manage.html", {'user':username, 'events':event_list})
 
 # 发布会名称搜索
@@ -41,9 +42,17 @@ def search_name(request):
 # 嘉宾管理
 @login_required
 def guest_manage(request):
-    guest_list = Guest.objects.all()
     username = request.session.get('user', '')
-    return render(request, "guest_manage.html", {'user':username, 'guests':guest_list})
+    guest_list = Guest.objects.all()    # 查询Guest表的所有数据
+    paginator = Paginator(guest_list, 2)    # 创建每页2条数据的分页器
+    page = request.GET.get('page')      # 通过GET请求获取当前要显示第几页的数据
+    try:
+        contacts = paginator.page(page)     # 获取第page页的数据
+    except PageNotAnInteger:                # 如果没有第page页，抛出PageNotAnInteger异常
+        contacts = paginator.page(1)        # 显示第1页的数据
+    except EmptyPage:                       # 如果超出页数范围，抛出Empty异常
+        contacts = paginator.page(paginator.num_pages)      # 显示最后一页的数据
+    return render(request, "guest_manage.html", {'user':username, 'guests':contacts})
 
 # 嘉宾搜索
 @login_required
@@ -51,4 +60,12 @@ def search_realname(request):
     username = request.session.get('user', '')
     search_realname = request.GET.get('realname', '')
     guest_list = Guest.objects.filter(realname__contains=search_realname)
-    return render(request, "guest_manage.html", {'user':username, 'guests':guest_list})
+    paginator = Paginator(guest_list, 2)  # 创建每页2条数据的分页器
+    page = request.GET.get('page')  # 通过GET请求获取当前要显示第几页的数据
+    try:
+        contacts = paginator.page(page)  # 获取第page页的数据
+    except PageNotAnInteger:  # 如果没有第page页，抛出PageNotAnInteger异常
+        contacts = paginator.page(1)  # 显示第1页的数据
+    except EmptyPage:  # 如果超出页数范围，抛出Empty异常
+        contacts = paginator.page(paginator.num_pages)  # 显示最后一页的数据
+    return render(request, "guest_manage.html", {'user': username, 'guests': contacts})
