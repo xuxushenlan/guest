7e9c001dcd07857b35dbc0b94b96fd2afb92f5cb
xuxushenlan <515945334@qq.com>
2019-06-24 15:22:24 +0800

    增加查询嘉宾接口，发布会签到接口

--- sign/views_if.py
+++ sign/views_if.py
@@ -40,7 +40,7 @@ def add_event(request):
 # 查询发布会接口
 def get_event_list(request):
     eid = request.GET.get('eid', '')                            # 发布会id
-    name = request.GET.get('eid', '')                           # 发布会名称
+    name = request.GET.get('name', '')                           # 发布会名称
 
     # 若id和name都为空，返回状态码和提示
     if eid == '' and name == '':
@@ -117,3 +117,85 @@ def add_guest(request):
     except IntegrityError:
         return JsonResponse({'status': 100026, 'message': 'the event guest phone number repeat'})
     return JsonResponse({'status': 200, 'message': 'add guest success'})
+
+
+# 查询嘉宾接口
+def get_guest_list(request):
+    eid = request.GET.get('id', '')
+    phone = request.GET.get('phone', '')
+
+    # 若eid为空，返回状态码和错误提示
+    if eid == '':
+        return JsonResponse({'status': 100021, 'message': 'eid can not empty'})
+    # 若eid不为空，phone为空，返回所有guest信息
+    if eid != '' and phone == '':
+        datas = []
+        results = Guest.objects.filter(event_id=eid)
+        if results:
+            for g in results:
+                guest = dict()
+                guest['realname'] = g.realname
+                guest['phone'] = g.phone
+                guest['email'] = g.email
+                guest['sign'] = g.sign
+                datas.append(guest)
+            return JsonResponse({'status': 200, 'message': 'success', 'data': datas})
+        else:
+            return JsonResponse({'status': 100022, 'message': 'query result is empty'})
+    # 若eid不为空且phone不为空，返回guest信息
+    if eid != '' and phone != '':
+        guest = {}
+        try:
+            result = Guest.objects.get(event_id=eid, phone=phone)
+        except ObjectDoesNotExist:
+            return JsonResponse({'status': 100022, 'message': 'query result is empty'})
+        else:
+            guest['realname'] = result.realname
+            guest['phone'] = result.phone
+            guest['email'] = result.email
+            guest['sign'] = result.sign
+            return JsonResponse({'status': 200, 'message': 'success', 'data': guest})
+
+
+# 发布会签到接口
+def user_sign(request):
+    eid = request.POST.get('eid', '')                   # 发布会id
+    phone = request.POST.get('phone', '')               # 嘉宾手机号
+    # 判断两个参数均不能为空
+    if eid == '' or phone == '':
+        return JsonResponse({'status': 100021, 'message': 'parameter error'})
+    # 判断发布会是否存在,不存在返回发布会id不存在
+    result = Event.objects.filter(id=eid)
+    if not result:
+        return JsonResponse({'status': 100022, 'message': 'event id null'})
+    # 判断发布会状态是否为True，不为True，说明发布会为关闭状态
+    result = Event.objects.get(id=eid).status
+    if not result:
+        return JsonResponse({'status': 100023, 'message': 'event status is not available'})
+    # 判断发布会是否已开始
+    event_time = Event.objects.get(id=eid).start_time           # 发布会时间
+    etime = str(event_time).split(".")[0]
+    timearray = time.strptime(etime, "%Y-%M-%D %H:%M:%S")
+    e_time = int(time.mktime(timearray))
+
+    now_time = str(time.time())                                 # 当前时间
+    ntime = now_time.split(".")[0]
+    n_time = int(ntime)
+
+    if n_time >= e_time:
+        return JsonResponse({'status': 100024, 'message': 'event has started'})
+    # 判断嘉宾手机号是否存在
+    result = Guest.objects.filter(phone=phone)
+    if not result:
+        return JsonResponse({'status': 100025, 'message': 'user phone null'})
+    # 判断嘉宾手机号与发布会id是否对应
+    result = Guest.objects.filter(event_id=eid, phone=phone)
+    if not result:
+        return JsonResponse({'status': 100026, 'message': 'user did not paticipate in the conference'})
+    # 判断嘉宾是否已签到
+    result = Guest.objects.get(event_id=eid, phone=phone).sign
+    if result:
+        return JsonResponse({'status': 100027, 'message': 'guest has sign in'})
+    else:
+        Guest.objects.filter(event_id=eid, phone=phone).update(sign='1')
+        return JsonResponse({'status': 100028, 'message': 'sign success'})
