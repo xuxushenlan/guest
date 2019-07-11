4fe162c418522898e67aeb176bed0a0369a37996
xuxushenlan <515945334@qq.com>
2019-07-11 17:54:04 +0800

    增加用户签名+时间戳（sign_time)和安全添加发布会接口

--- sign/views_if_sec.py
+++ sign/views_if_sec.py
@@ -3,14 +3,11 @@ from django.views.decorators.csrf import csrf_exempt
 from sign.models import Event, Guest
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.db.utils import IntegrityError
-import time
+import time,hashlib,base64
 from django.contrib import auth as django_auth
-import base64
 
 
 # 用户认证
-
-
 def user_auth(request):
     # request.META是一个Python字典，包含了本次HTTP请求的Header信息，例如用户认证、IP地址和user - Agent等。
     # HTTP_AUTHORIZATION用于获取HTTP认证数据，如Basic YWRtaW46YWRtaW4xMjMNTY=
@@ -77,3 +74,79 @@ def sec_get_event_list(request):
         else:
             return JsonResponse({'status': 10022, 'message': 'query result is empty'})
 
+# 用户签名+时间戳
+def sign_time(request):
+    # 判断客户端请求是否为POST，是则获取客户端时间戳和签名，否则返回'error'
+    if request.method == 'POST':
+        client_time = request.POST.get('time', '')  # 客户端时间戳
+        client_sign = request.POST.get('sign', '')  # 客户端签名
+    else:
+        return 'error'
+    # 判断客户端时间戳和签名不能为空，为空则返回'sign null'
+    if client_sign == '' or client_time == '':
+        return 'sign null'
+
+    # 服务器时间
+    now_time = time.time()
+    server_time = str(now_time).split('.')[0]
+    # 获取时间差
+    time_difference = int(server_time) - int(client_time)
+
+    # 若服务器时间戳-客户端时间戳大于等于60秒，则返回'timeout'
+    if time_difference >= 60:
+        return 'timeout'
+
+    # 签名检查，将api_key和客户端发送的时间戳喝到一起，通过MD5生成新的加密字符串作为服务器端的sign参数，即server_sign
+    md5 = hashlib.md5()
+    sign_str = client_time + '&Guest-Bugmaster'     # api_key密钥字符串为'&Guest-Bugmaster'
+    sign_bytes_utf8 = sign_str.encode(encoding='utf-8')
+    md5.update(sign_bytes_utf8)
+    server_sign = md5.hexdigest()
+
+    # 若服务器签名与客户端签名一致，则返回'sign success',否则返回'sign fail'
+    if server_sign != client_sign:
+        return 'sign fail'
+    else:
+        return 'sign success'
+
+# 添加发布会接口--增加签名+时间戳
+@csrf_exempt    # django 的POST接口需要CSRF验证，添加此装饰器豁免验证
+def sec_add_event(request):
+    sign_result = sign_time(request)
+    if sign_result == 'error':
+        return JsonResponse({'status': 10011, 'message': 'request error'})
+    if sign_result == 'sign null':
+        return JsonResponse({'status': 10012, 'message': 'client sign null'})
+    if sign_result == 'timeout':
+        return JsonResponse({'status': 10013, 'message': 'client sign timeout'})
+    if sign_result == 'sign fail':
+        return JsonResponse({'status': 10014, 'message': 'client sign error'})
+
+    eid = request.POST.get('eid', '')                           # 发布会id
+    name = request.POST.get('name', '')                         # 发布会名称
+    limit = request.POST.get('limit', '')                       # 限制人数
+    status = request.POST.get('status', '')                     # 状态
+    address = request.POST.get('address', '')                   # 地址
+    start_time = request.POST.get('start_time', '')             # 开始时间
+
+    # 若参数为空，则返回状态码100021，错误信息parameter error
+    if eid == '' or name == '' or limit == '' or address == '' or start_time == '':
+        return JsonResponse({'status': 10021, 'message': 'parameter error'})
+    # 判断发布会id和名称是否存在，若存在返回状态码和提示
+    result = Event.objects.filter(id=eid)
+    if result:
+        return JsonResponse({'status': 10022, 'message': 'Event id already exists'})
+    result = Event.objects.filter(name=name)
+    if result:
+        return JsonResponse({'status': 10023, 'message': 'Event name already exists'})
+    # 若发布会status为空，置默认值1
+    if status == '':
+        status = 1
+    # 插入数据，若日期格式错误，返回状态码和提示
+    try:
+        Event.objects.create(id=eid, name=name, limit=limit, status=int(status), address=address,
+                             start_time=start_time)
+    except ValidationError:
+        error = 'start_time format error. It must be in YYYY-MM-DD HH:MM:SS format.'
+        return JsonResponse({'status': 10024, 'message': error})
+    return JsonResponse({'status': 200, 'message': 'add event success'})
